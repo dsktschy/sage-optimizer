dddee3a5920307c09193dd84f33376e9f5880b7c
dai <chamestica@gmail.com>
2018-12-04 23:20:57 +0900

    Describe processes

---
+++ sage-optimizer.sh
@@ -0,0 +1,11 @@
+# Ask directory name of theme to be created
+# Ask site domain of local development hostname
+# Ask path to .crt file for SSL
+# Ask path to .key file for SSL
+# Detect path to directory contains this script
+# Create theme based Sage
+# Start sub shell
+  # Move to theme directory
+  # Create .env from variables
+  # Install and uninstall Node modules
+  # Replace and remove files
